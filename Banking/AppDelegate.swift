c34f1b24ac530d77cb934523556b67b43c75264c
FFFFSU <christiannico04@gmail.com>
2022-04-06 12:19:56 +0700

    Resolve conflict

--- Banking/AppDelegate.swift
+++ Banking/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         // Master update 2
         // Master update 3
+        // Feature 1
         return true
     }
 
